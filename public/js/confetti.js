86ff2f8a33fa13c666b72132675972b27d9fc7c9
Nathaniel <nathaniel.habtegerge@ucalgary.ca>
2022-04-08 10:03:02 -0600

    confetti works for half the screen

--- public/js/confetti.js
+++ public/js/confetti.js
@@ -49,7 +49,7 @@ var removeConfetti; //call to stop the confetti animation and remove all confett
 		if (canvas === null) {
 			canvas = document.createElement("canvas");
 			canvas.setAttribute("id", "confetti-canvas");
-			canvas.setAttribute("style", "display:block;z-index:9999;pointer-events:none");
+			// canvas.setAttribute("style", "position:absolute; display:block; z-index:9999; pointer-events:none");
 			document.body.appendChild(canvas);
 			canvas.width = width;
 			canvas.height = height;
